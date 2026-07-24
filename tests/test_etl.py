839891d099f390f0f51cedf68f41392efd4d284c
rgudivada21 <rgudivada21@gmail.com>
2026-07-24 16:14:49 +0530

    Update test_etl.py

--- tests/test_etl.py
+++ tests/test_etl.py
@@ -1,29 +1,21 @@
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 from pyspark.sql import SparkSession
 from src.etl import transform_data
-
 
 spark = SparkSession.builder.master("local[*]").appName("Test").getOrCreate()
 
 
 def test_total_amount_column():
-    data = [
-        (1, "Laptop", "Electronics", 2, 1000)
-    ]
-
-    columns = [
-        "Order_ID",
-        "Product",
-        "Category",
-        "Quantity",
-        "Price"
-    ]
+    data = [(1, "Laptop", "Electronics", 2, 1000)]
+    columns = ["Order_ID", "Product", "Category", "Quantity", "Price"]
 
     df = spark.createDataFrame(data, columns)
 
     result = transform_data(df)
 
     assert "Total_Amount" in result.columns
-
-    total = result.collect()[0]["Total_Amount"]
-
-    assert total == 2000
+    assert result.collect()[0]["Total_Amount"] == 2000
