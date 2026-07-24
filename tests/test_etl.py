b37a7488a3f6f8f3ad3bacf2d35fdb0dfb0be0a8
rgudivada21 <rgudivada21@gmail.com>
2026-07-24 16:05:36 +0530

    Update test_etl.py
    add unit test

--- tests/test_etl.py
+++ tests/test_etl.py
@@ -1,1 +1,29 @@
+from pyspark.sql import SparkSession
+from src.etl import transform_data
 
+
+spark = SparkSession.builder.master("local[*]").appName("Test").getOrCreate()
+
+
+def test_total_amount_column():
+    data = [
+        (1, "Laptop", "Electronics", 2, 1000)
+    ]
+
+    columns = [
+        "Order_ID",
+        "Product",
+        "Category",
+        "Quantity",
+        "Price"
+    ]
+
+    df = spark.createDataFrame(data, columns)
+
+    result = transform_data(df)
+
+    assert "Total_Amount" in result.columns
+
+    total = result.collect()[0]["Total_Amount"]
+
+    assert total == 2000
